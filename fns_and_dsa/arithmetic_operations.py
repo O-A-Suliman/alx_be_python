ee80dc5ffc48f0dc883b14c98ecc3abb2cc71b24
O-A-Suliman <ayahyam18@gmail.com>
2025-08-02 16:48:55 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,7 +6,10 @@ def perform_operation(num1,num2,operation):
     elif operation=="multiply":
         return num1 *num2
     elif operation=="divide":
-        return num1/num2
+        if num2==0:
+            print("divide by zero scenario")
+        else:
+            return num1/num2
 
 def main():
     print("Arithmetic Operations")
