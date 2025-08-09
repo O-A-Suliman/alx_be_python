c7ae4c98b417f1ad2a90a73864e7ad7b6d1d258a
O-A-Suliman <ayahyam18@gmail.com>
2025-08-09 14:27:20 +0300

    Update class_static_methods_demo.py

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -8,6 +8,4 @@ class Calculator:
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
-        product = a * b
-        print(f"The product is: {product}")
-
+        print(f"The product is: {a * b}")
